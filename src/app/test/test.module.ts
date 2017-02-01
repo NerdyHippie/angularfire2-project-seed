15203778ac711e0477034eccc38e116635e92413
joshorvis <jorvis@nerdyhippie.com>
2017-02-01 01:31:32 -0500

    Added Material Icons and Font-Awesome icons in Material Wrapper

--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -12,6 +12,8 @@ import { MdCheckboxModule } from '@angular2-material/checkbox'
 import { MdTooltipModule } from '@angular2-material/tooltip';
 import { MdSliderModule } from '@angular2-material/slider';
 
+import { MdIconModule, MdIconRegistry } from '@angular2-material/icon';
+
 import { TestRouting,TestRouteComponents } from './test.routing';
 
 
@@ -28,8 +30,13 @@ import { TestRouting,TestRouteComponents } from './test.routing';
 	  MdRadioModule.forRoot(),
 	  MdCheckboxModule.forRoot(),
 	  MdTooltipModule.forRoot(),
-	  MdSliderModule.forRoot()
+	  MdSliderModule.forRoot(),
+	  MdIconModule.forRoot()
   ],
   declarations: [TestRouteComponents]
 })
-export class TestModule { }
+export class TestModule {
+	constructor(mdIconRegistry: MdIconRegistry) {
+		mdIconRegistry.registerFontClassAlias('fontawesome', 'fa');
+	}
+}
