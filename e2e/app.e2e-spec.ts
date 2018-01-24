41645394d9a9faf4988348acb5bbac846179a20a
joshorvis <jorvis@nerdyhippie.com>
2018-01-24 08:22:25 -0500

    Starting to update to current versions of libraries

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { StarterProjectPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('ng-cli App', function() {
-  let page: StarterProjectPage;
+describe('groovy-task App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new StarterProjectPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class StarterProjectPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
